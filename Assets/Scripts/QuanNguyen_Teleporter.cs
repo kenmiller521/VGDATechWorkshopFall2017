04a2aa07a9996c1cf241aac1d0da1173fdb709fd
quandom90 <quanlynguyen90@gmail.com>
2017-09-15 12:59:05 -0700

    WorkShop Finish

---
+++ Assets/Scripts/QuanNguyen_Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuanNguyen_Teleporter : MonoBehaviour {
+    public GameObject player;
+    public float followOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Platform"))
+        {
+
+        }
+    }
+}
