6927b723911f5a7d2f2bda5ed8a7d7e574483bc7
Ken Miller <ken.miller521@gmail.com>
2017-09-15 12:58:27 -0700

    Workshop Finished

---
+++ Assets/Scripts/KenMiller_PlatformTeleporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KenMiller_PlatformTeleporter : MonoBehaviour {
+    public GameObject player;
+    public float followOffset;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x-followOffset, player.transform.position.y, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Contains("Platform"))
+        {
+            collision.gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x+9, collision.gameObject.transform.position.y,0);
+        }
+    }
+}
