cb0b073403156347bf10b9687dfc099a36a789e6
WHellhaus <wolfgang.hellicksons@gmail.com>
2017-09-15 13:00:47 -0700

    Workshop
    Workshop

---
+++ Assets/Scripts/WOlfgangHellicksonSabelhaus_PlatformTeleporter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WOlfgangHellicksonSabelhaus_PlatformTeleporter : MonoBehaviour {
+
+	public GameObject player;
+	public float followOffset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = new Vector3 (player.transform.position.x - followOffset, player.transform.position.y, 0);
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.name.Contains("Platform"))
+		{
+			collision.transform.position = new Vector3 (collision.transform.position.x + 9, collision.transform.position.y, collision.transform.position.z);
+		}
+	}
+}
